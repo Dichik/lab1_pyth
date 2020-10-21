9941934e0eead9f93e6ba1411d4e22ae3e3b8d7d
User <User@DESKTOP-HQ33TUL>
2020-10-21 10:48:12 +0300

    lll

--- lab1_pyth/lab1_pyth.py
+++ lab1_pyth/lab1_pyth.py
@@ -1,13 +1,9 @@
 import math
 
-print('Enter coordinate of x1: ')
-x1 = int(input())
-print('Enter coordinate of y1: ')
-y1 = int(input())
-print('Enter coordinate of x2: ')
-x2 = int(input())
-print('Enter coordinate of y2: ')
-y2 = int(input())
+x1 = float(input('Enter coordinate of x1: '))
+y1 = float(input('Enter coordinate of y1: '))
+x2 = float(input('Enter coordinate of x2: '))
+y2 = float(input('Enter coordinate of y2: '))
 ans = float(math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) ) )
 print('The distance between two poits equal to: ')
 print(ans)
